9f856bfd042bc408b5e357088ab96e288b8f7125
Dalleau Sara <sara.dalleau@gmail.com>
2026-02-06 17:04:28 +0100

    ajout structure minimale

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/'
 
 function App() {
 
