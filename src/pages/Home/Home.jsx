6a4e4383329eb32adf3d0475ed2141c350ff331f
Dalleau Sara <sara.dalleau@gmail.com>
2026-02-19 14:27:18 +0100

    responsive page Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,30 @@
 import Banner from "../../components/Banner/index.jsx"
-import Card from "../../components/Card/index.jsx"
-import logements from '../../data/logements.json'
+import Card from "../../components/Card"
+import logements from "../../data/logements.json"
 import bannerHome from "../../assets/images/banner.png"
 import "./Home.scss"
-
 
 function Home() {
   return (
     <div className="home">
-    <Banner
-      img={bannerHome}
-      title="Chez vous, partout et ailleurs"
-    />
-    
       <div className="home-content">
-      <div className="cards-container">
-        {logements.map((logement) => (
-          <Card 
-            key={logement.id}
-            id={logement.id}
-            title={logement.title}
-            cover={logement.cover}>
-          </Card>
-        ))}
+        <Banner
+          img={bannerHome}
+          title="Chez vous, partout et ailleurs"
+        />
+
+        <div className="cards-container">
+          {logements.map((logement) => (
+            <Card
+              key={logement.id}
+              id={logement.id}
+              title={logement.title}
+              cover={logement.cover}
+            />
+          ))}
         </div>
       </div>
-      </div>
+    </div>
   )
 }
 
